96930ffd4011a2d5570b5af08b9a5a5d362bbff8
jongwon Kim <jong960807@gmail.com>
2022-02-03 01:19:14 +0900

    update:세팅파일

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['*','0.0.0.0','192.168.0.2']
 ALLOWED_HOSTS = ['192.168.0.2','*','0.0.0.0']
