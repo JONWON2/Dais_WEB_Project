12c26eaa95c0c6e21237d8c99334d50a49443ad6
jongwon Kim <jong960807@gmail.com>
2022-02-02 17:07:09 +0900

    DB 저장 전까지 완료

--- contentsapp/urls.py
+++ contentsapp/urls.py
@@ -7,8 +7,16 @@ app_name = 'contentsapp'
 
 urlpatterns = [
     path('intro/', views.intro_page,name='intro'),
-    path('meetup/', views.meetup_page,name='meetup'),
-    path('proud/', views.proud_page,name='proud'),
-    path('study/', views.study_page,name='study'),
-    path('notice/', views.notice_page,name='notice'),
+    path('meetup/', views.meetup_page.as_view(),name='meetup'),
+    path('meetup/write/', views.meetup_write_page.as_view(),name='meetup_write'),
+    
+    path('proud/', views.proud_page.as_view(),name='proud'),
+    path('proud/write/', views.proud_write_page.as_view(),name='proud_write'),
+
+    path('study/', views.study_page.as_view(),name='study'),
+    path('study/write/', views.study_write_page.as_view(),name='study_write'),
+
+    path('notice/', views.notice_page.as_view(),name='study'),
+    path('notice/write/', views.notice_write_page.as_view(),name='notice_write'),
+
 ]
--- contentsapp/views.py
+++ contentsapp/views.py
@@ -3,29 +3,140 @@ from django.shortcuts import render
 from django.test import RequestFactory
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseForbidden
 from django.urls import reverse, reverse_lazy
-
+from django.views import View  
 
 # Create your views here.
 def intro_page(request):
     return render(request, 'contentsapp/lab_intro.html') 
 
-def meetup_page(request):
-    year = request.GET.get('year')
-    request.session["year"] = str(year)
-    return render(request, 'contentsapp/lab_meetup.html',{"year": str(year)})
+## 초기에 화면 View를 위해 사용한 함수.
+# def meetup_page(request):
+#     year = request.GET.get('year')
+#     request.session["year"] = str(year)
+#     return render(request, 'contentsapp/lab_meetup.html',{"year": str(year)})
+# def proud_page(request):
+#     year = request.GET.get('year')
+#     return render(request, 'contentsapp/lab_proud.html',{"year": str(year)}) 
+# def study_page(request):
+#     category = request.GET.get('category')
+#     return render(request, 'contentsapp/lab_study.html',{'category':category}) 
+# def notice_page(request):
+#     return render(request, 'contentsapp/lab_notice.html') 
+
+## 기능을 추가하기위해 GET과 POST에 대해 구분 지은 클래스 View
+class meetup_page(View):
+    def get(self, request, *args, **kwargs):
+        year = request.GET.get('year')
+        request.session["year"] = str(year)
+        return render(request, 'contentsapp/lab_meetup.html',{"year": str(year)})
+    def post(self, request, *args, **kwargs):
+        year = request.GET.get('year')[:-1]
+        request.session["year"] = str(year)
+        return HttpResponseRedirect(reverse('contentsapp:meetup_write'))
+
+        # return render(request, 'contentsapp/write_meetup.html',{"year": str(year)})
+class meetup_write_page(View):
+    def get(self, request, *args, **kwargs):
+        year = request.session["year"]
+        print("GET으로 받음?")
+        return render(request, 'contentsapp/write_meetup.html')
+
+    def post(self, request, *args, **kwargs):
+        print("POST으로 받음?")
+        # 이전 버튼을 눌렀을때
+        if request.POST.get('before',False):
+            year = request.session["year"]
+            return render(request, 'contentsapp/lab_meetup.html',{"year": str(year)+"/"})
+        # 완료 버튼을 눌렀을때
+        if request.POST.get('finish',False):
+            year = request.session["year"]
+            return render(request, 'contentsapp/lab_meetup.html',{"year": str(year)+"/"})
+        return  HttpResponseRedirect(reverse('app:main'))
+
+#############################################################
+class proud_page(View):
+    def get(self, request, *args, **kwargs):
+        year = request.GET.get('year')
+        request.session["year"] = str(year)
+        return render(request, 'contentsapp/lab_proud.html',{"year": str(year)})
+    def post(self, request, *args, **kwargs):
+        year = request.GET.get('year')[:-1]
+        request.session["year"] = str(year)
+        return HttpResponseRedirect(reverse('contentsapp:proud_write'))
+
+class proud_write_page(View):
+    def get(self, request, *args, **kwargs):
+        year = request.session["year"]
+        print("GET으로 받음?")
+        return render(request, 'contentsapp/write_proud.html')
+
+    def post(self, request, *args, **kwargs):
+        print("POST으로 받음?")
+        # 이전 버튼을 눌렀을때
+        if request.POST.get('before',False):
+            year = request.session["year"]
+            return render(request, 'contentsapp/lab_proud.html',{"year": str(year)+"/"}) 
+        # 완료 버튼을 눌렀을때
+        if request.POST.get('finish',False):
+            year = request.session["year"]
+            return render(request, 'contentsapp/lab_proud.html',{"year": str(year)+"/"}) 
+        return  HttpResponseRedirect(reverse('app:main'))
 
 
-def proud_page(request):
-    year = request.GET.get('year')
-    return render(request, 'contentsapp/lab_proud.html',{"year": str(year)}) 
+#############################################################
+class study_page(View):
+    def get(self, request, *args, **kwargs):
+        category = request.GET.get('category')
+        request.session["category"] = category
+        return render(request, 'contentsapp/lab_study.html',{"category": str(category)})
 
-def study_page(request):
-    category = request.GET.get('category')
+    def post(self, request, *args, **kwargs):
+        category = request.GET.get('category')
+        request.session["category"] = category
+        return HttpResponseRedirect(reverse('contentsapp:study_write'))
 
-    return render(request, 'contentsapp/lab_study.html',{'category':category}) 
+class study_write_page(View):
+    def get(self, request, *args, **kwargs):
+        print("GET으로 받음?")
+        return render(request, 'contentsapp/write_study.html')
 
-def notice_page(request):
-    return render(request, 'contentsapp/lab_notice.html') 
+    def post(self, request, *args, **kwargs):
+        print("POST으로 받음?")
+        # 이전 버튼을 눌렀을때
+        if request.POST.get('before',False):
+            category = request.session["category"]
+            return render(request, 'contentsapp/lab_study.html',{"category": category}) 
+        # 완료 버튼을 눌렀을때
+        if request.POST.get('finish',False):
+            category = request.session["category"]
+            return render(request, 'contentsapp/lab_study.html',{"category": category}) 
+
+        return  HttpResponseRedirect(reverse('app:main'))
+
+#############################################################
+
+
+class notice_page(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'contentsapp/lab_notice.html')
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponseRedirect(reverse('contentsapp:notice_write'))
+
+class notice_write_page(View):
+    def get(self, request, *args, **kwargs):
+        print("GET으로 받음?")
+        return render(request, 'contentsapp/write_notice.html')
+
+    def post(self, request, *args, **kwargs):
+        print("POST으로 받음?")
+        # 이전 버튼을 눌렀을때
+        if request.POST.get('before',False):
+            return render(request, 'contentsapp/lab_notice.html') 
+        # 완료 버튼을 눌렀을때
+        if request.POST.get('finish',False):
+            return render(request, 'contentsapp/lab_notice.html') 
+        return  HttpResponseRedirect(reverse('app:main'))
 
 
 
